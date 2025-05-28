91e254afabe3240844fa8511a68e6778b22c2423
parth thanth <thanthparth3@gmail.com>
2025-05-28 13:07:33 +0530

    boom

--- recursion/powerFun.java
+++ recursion/powerFun.java
@@ -1,18 +1,19 @@
 package recursion;
 import java.util.*;
+
 public class powerFun {
     public static void main(String[] args){
-        Scanner sc=new Scanner (System.in);
-        int num=sc.nextInt();
-        int power=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int power = sc.nextInt();
         System.out.println(powerFunction(num, power));
     }
-    static int powerFunction (int num,int  power){
-        if(power==0){
-            return num;
-        }else{
-            return num * powerFunction(num, power-1);
+
+    static int powerFunction(int num, int power){
+        if(power == 0){
+            return 1; 
+        } else {
+            return num * powerFunction(num, power - 1);
         }
     }
-    
 }
