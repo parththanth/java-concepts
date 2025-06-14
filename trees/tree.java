fee1e4561de578e9079229f32d62d3b37fcc52f7
parth thanth <thanthparth3@gmail.com>
2025-06-14 09:29:23 +0530

    done

---
+++ trees/tree.java
@@ -0,0 +1,31 @@
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class tree {
+    Node root;
+
+    public void travels(Node node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            travels(node.left);
+            travels(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        tree tree = new tree();
+        tree.root = new Node(10);
+        tree.root.left = new Node(20);
+        tree.root.right = new Node(30);
+        System.out.println("\nBinary Tree");
+        tree.travels(tree.root);
+        
+    }
+}
